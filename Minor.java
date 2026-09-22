31be01d481ee87ecddf015baae4b0587998b6525
ashna130 <ashnavelikath@gmail.com>
2026-09-22 12:59:26 -0400

    Update Minor.java

--- Minor.java
+++ Minor.java
@@ -1,6 +1,24 @@
 public class Minor {
 
-
+  private String[] minors = ["Biomedical Engineering", "Computer Science", "Cybersecurity", "Human-Computer Interactions", "Artificial Intelligence", "Engineering Science and Mechanics", "Green Engineering", "Naval Engineering", "Nuclear Engineering"];
+  // biomedical
+  private String[] biomed = [];
+  // cs
+  private String[] cs = [];
+  // cybersecurity
+  private String[] cyber = [];
+  // human-computer interactions
+  private String[] hci = [];
+  // ai
+  private String[] ai = [];
+  // engineering science and mechanics
+  private String[] ecm = [];
+  // green engineering
+  private String[] green = [];
+  // naval
+  private String[] naval = [];
+  // nuclear
+  private String[] nuclear = [];
   
   public void includeMinor(String minor) {
     // uses the minor to find what classes need to be added, doesn't add classes already in array
