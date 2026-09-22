8cecb3987e2f199ddd6cfdc1401a8f16540fb506
diyas04 <diyas04@vt.edu>
2026-09-22 13:41:16 -0400

    created dropdown, read comment on how to implement

--- Schedule.java
+++ Schedule.java
@@ -1,6 +1,189 @@
 public class Schedule {
+  
+import java.awt.Color;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
   public static void main(String[] args) {
 
+
+
+    // DROPDOWN
+    Frame frame = new JFrame("Engineering Schedule Maker");
+        JPanel panel = new JPanel();
+
+        // greyed out first selection
+        DefaultListCellRenderer grayPlaceholderRenderer =
+            new DefaultListCellRenderer() {
+                @Override
+                public java.awt.Component getListCellRendererComponent(
+                    JList<?> list,
+                    Object value,
+                    int index,
+                    boolean isSelected,
+                    boolean cellHasFocus) {
+
+                    super.getListCellRendererComponent(list, value, index,
+                        isSelected, cellHasFocus);
+
+                    if (index == 0) {
+                        setForeground(Color.GRAY);
+                    }
+
+                    return this;
+                }
+            };
+
+        // the "choose minor" label
+        JLabel mLabel = new JLabel("Choose Minor:");
+
+        // Year dropdown
+        String[] yearChoices = { "Select...", "First Year", "Second Year",
+            "Third Year", "Fourth Year" };
+
+        JComboBox<String> yearDropdown = new JComboBox<>(yearChoices);
+        yearDropdown.setRenderer(grayPlaceholderRenderer);
+
+        // Major dropdown
+        String[] majorChoices = { "Select...", "Aerospace and Ocean",
+            "Biological Systems", "Biomedical", "Building Construction",
+            "Chemical", "Civil", "Computer Engineering", "Computer Science",
+            "Construction", "Electrical", "Industrial and Systems",
+            "Materials Science", "Mechanical", "Mining" };
+
+        JComboBox<String> majorDropdown = new JComboBox<>(majorChoices);
+        majorDropdown.setRenderer(grayPlaceholderRenderer);
+
+        // Minor? dropdown
+        String[] minorQuestionChoices = { "Select...", "Yes", "No" };
+
+        JComboBox<String> minorQuestionDropdown = new JComboBox<>(
+            minorQuestionChoices);
+        minorQuestionDropdown.setRenderer(grayPlaceholderRenderer);
+
+        // Minor dropdown
+        String[] minorChoices = { "Select...", "Biomedical Engineering",
+            "Computer Science", "Cybersecurity", "Human-Computer Interaction",
+            "Artificial Intelligence", "Engineering Science and Mechanics",
+            "Green Engineering", "Naval Engineering", "Nuclear Engineering" };
+
+        JComboBox<String> minorDropdown = new JComboBox<>(minorChoices);
+        minorDropdown.setRenderer(grayPlaceholderRenderer);
+
+        JButton enterButton = new JButton("Enter");
+
+        // Initially hide the minor selection
+        minorDropdown.setVisible(false);
+        mLabel.setVisible(true);
+
+        // Add components to panel
+        panel.add(new JLabel("Year:"));
+        panel.add(yearDropdown);
+
+        panel.add(new JLabel("Major:"));
+        panel.add(majorDropdown);
+
+        panel.add(new JLabel("Take a Minor?"));
+        panel.add(minorQuestionDropdown);
+
+        panel.add(enterButton);
+
+        // Minor question listener
+        minorQuestionDropdown.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (minorQuestionDropdown.getSelectedIndex() == 0) {
+                    return;
+                }
+
+                String selected = (String)minorQuestionDropdown
+                    .getSelectedItem();
+
+                if (selected.equals("Yes")) {
+                    panel.remove(enterButton);
+                    panel.add(mLabel);
+                    panel.add(minorDropdown);
+                    panel.add(enterButton);
+
+                    mLabel.setVisible(true);
+                    minorDropdown.setVisible(true);
+
+                }
+                else {
+                    mLabel.setVisible(false);
+                    minorDropdown.setVisible(false);
+                }
+
+                // Resize the window to fit the now visible component
+                frame.pack();
+            }
+        });
+        
+        
+         // Where the inputs of the dropdowns are actually recorded
+         
+        enterButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                if (yearDropdown.getSelectedIndex() == 0 || majorDropdown
+                    .getSelectedIndex() == 0 || minorQuestionDropdown
+                        .getSelectedIndex() == 0) {
+
+                    JOptionPane.showMessageDialog(frame,
+                        "Please complete all required selections.");
+                    return;
+                }
+
+                if (minorQuestionDropdown.getSelectedItem().equals("Yes")
+                    && minorDropdown.getSelectedIndex() == 0) {
+
+                    JOptionPane.showMessageDialog(frame,
+                        "Please select a minor.");
+                    return;
+                }
+
+              //test code to see if everything is selected
+               // System.out.println("Everything is selected!");
+
+              /*
+              * HOW TO ACCESS DROPDOWN OUTPUTS
+              * for the console outputs below, they just take the String 
+              * value from the dropdown and output that. If you want to
+              * use a .equals("") to designate majors that could work.
+              * You can also use .getSelectedIndex() which will give
+              * you the index of the user selected item. Note that if 
+              * you are using this, the "Select..." item is at index 0
+              * then the actual majors and minors are at index 1 and up
+              */
+                String year = (String)yearDropdown.getSelectedItem();
+                System.out.println("Year selected: " + year);
+
+                String major = (String)majorDropdown.getSelectedItem();
+                System.out.println("Major selected: " + major);
+
+                String minorQuestion = (String)minorQuestionDropdown
+                    .getSelectedItem();
+                System.out.println("Minor? " + minorQuestion);
+
+                if (minorQuestionDropdown.getSelectedIndex() == 2) {
+                    System.out.println("No minor selected");
+                }
+                else if (minorQuestionDropdown.getSelectedIndex() == 1) {
+                    String minor = (String)minorDropdown.getSelectedItem();
+                    System.out.println("Minor selected: " + minor);
+                }
+
+            }
+        });
+
+        // JFrame setup
+        frame.add(panel);
+        frame.pack();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    
   }
 
 }
