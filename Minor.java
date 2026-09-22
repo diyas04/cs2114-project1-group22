f5e39ef1416a655f18401118569de7a4cacfd4c3
ashna130 <ashnavelikath@gmail.com>
2026-09-22 13:08:23 -0400

    Update Minor.java

--- Minor.java
+++ Minor.java
@@ -1,24 +1,32 @@
 public class Minor {
 
-  private String[] minors = ["Biomedical Engineering", "Computer Science", "Cybersecurity", "Human-Computer Interactions", "Artificial Intelligence", "Engineering Science and Mechanics", "Green Engineering", "Naval Engineering", "Nuclear Engineering"];
+  private String[] minors = {"Biomedical Engineering", 
+                             "Computer Science", 
+                             "Cybersecurity", 
+                             "Human-Computer Interactions", 
+                             "Artificial Intelligence", 
+                             "Engineering Science and Mechanics", 
+                             "Green Engineering", 
+                             "Naval Engineering", 
+                             "Nuclear Engineering"};
   // biomedical
-  private String[] biomed = [];
+  private String[] biomed = {"BMES 2104", "BMES 4064"};
   // cs
-  private String[] cs = [];
+  private String[] cs = {"CS 1114", "CS 2114", "MATH 2534", "CS 2505", "CS 3114"};
   // cybersecurity
-  private String[] cyber = [];
+  private String[] cyber = {"ECE 4560", "CS 4264", "ECE/CS 5560"};
   // human-computer interactions
-  private String[] hci = [];
+  private String[] hci = {"HIST/SOC/STS 2604", "COMM 2084", "CS 3724"};
   // ai
-  private String[] ai = [];
+  private String[] ai = {"CS 1114", "CS 2104", "CS 3804"};
   // engineering science and mechanics
-  private String[] ecm = [];
+  private String[] esm = {"ESM 2104", "ESM 2204", "ESM 2304"};
   // green engineering
-  private String[] green = [];
+  private String[] green = {"ENGR 3124", "ENGR 4134"};
   // naval
-  private String[] naval = [];
+  private String[] naval = {"AOE 2204", "AOE 4264", "AOE 4244"};
   // nuclear
-  private String[] nuclear = [];
+  private String[] nuclear = {"MATH 2214", "NSEG 3145", "NSEG 3146", "NSEG 3604"};
   
   public void includeMinor(String minor) {
     // uses the minor to find what classes need to be added, doesn't add classes already in array
