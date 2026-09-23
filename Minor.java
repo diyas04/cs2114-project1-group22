eac01e343600e7b5c9e0d0ef80f1fdc82ac4a242
ashna130 <ashnavelikath@gmail.com>
2026-09-22 22:09:09 -0400

    Update Minor.java

--- Minor.java
+++ Minor.java
@@ -27,18 +27,91 @@ public class Minor {
   private String[] naval = {"AOE 2204", "AOE 4264", "AOE 4244"};
   // nuclear
   private String[] nuclear = {"MATH 2214", "NSEG 3145", "NSEG 3146", "NSEG 3604"};
-  
+
+  /**
+    * Adds minor classes 
+    */
   public void includeMinor(String minor) {
     // uses the minor to find what classes need to be added, doesn't add classes already in array
     if (minor.equals(null)) {
       return;
     }
-    if (minor.equals(minors[1])) {
-
-    }
   }
 
+  /**
+    *
+    */
   public void addMinorCourses(String[] minor) {
     
   }
+
+   /**
+     * Returns the currently selected third-year courses.
+     */
+    public String[] getCourses() {
+        return courses;
+    }
+
+
+    /**
+     * Adds a course to the current schedule.
+     *
+     * @return true if the course was added, false otherwise
+     */
+    public boolean addCourse(String courseName) {
+
+        if (courseName == null) {
+            return false;
+        }
+
+        for (String course : courses) {
+            if (course.equalsIgnoreCase(courseName)) {
+                return false;
+            }
+        }
+
+        String[] newCourses = new String[courses.length + 1];
+        for (int i = 0; i < courses.length; i++) {
+            newCourses[i] = courses[i];
+        }
+        newCourses[newCourses.length - 1] = courseName;
+        courses = newCourses;
+
+        return true;
+    }
+
+
+    /**
+     * Removes a course from the current schedule.
+     *
+     * @return true if the course was removed, false otherwise
+     */
+    public boolean removeCourse(String courseName) {
+
+        if (courseName == null) {
+            return false;
+        }
+
+        for (int i = 0; i < courses.length; i++) {
+
+            if (courses[i].equalsIgnoreCase(courseName)) {
+
+                String[] newCourses = new String[courses.length - 1];
+
+                for (int j = 0, k = 0; j < courses.length; j++) {
+
+                    if (j != i) {
+                        newCourses[k] = courses[j];
+                        k++;
+                    }
+                }
+
+                courses = newCourses;
+
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
