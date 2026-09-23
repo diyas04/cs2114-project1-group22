2f0f84fade7cc9b54cca6389d8be9f0e80ad7bd5
ashna130 <ashnavelikath@gmail.com>
2026-09-23 00:32:09 -0400

    Update Minor.java

--- Minor.java
+++ Minor.java
@@ -10,23 +10,124 @@ public class Minor {
                              "Naval Engineering", 
                              "Nuclear Engineering"};
   // biomedical
-  private String[] biomed = {"BMES 2104", "BMES 4064", "BMES 3004", "BMES 3124", "BMES 3134", "BMES 3144", "BMES 3154", "BMES 3164", "BMES 3224", "BMES 3704", "BMES 4034", "BMES 4084", "BMES 4134", "BMES 4154"};
-  // cs
-  private String[] cs = {"CS 1114", "CS 2114", "MATH 2534", "CS 2505", "CS 3114", "CS 3214", "CS 4104"};
-  // cybersecurity
-  private String[] cyber = {"ECE 4560", "CS 4264", "ECE/CS 5560", "ECE 3564", "CS 3214", "ACIS 4684", "CS 4274", "ECE 4944"};
-  // human-computer interactions
-  private String[] hci = {"HIST/SOC/STS 2604", "COMM 2084", "CS 3724", "IDS 2214", "CS 3714", "CS 4774"};
-  // ai
-  private String[] ai = {"CS 1114", "CS 2104", "CS 3804", "PHIL 3334", "GEOG 4094", "MKTG 4114"};
-  // engineering science and mechanics
-  private String[] esm = {"ESM 2104", "ESM 2204", "ESM 2304", "ESM 3234", "ESM 3124", "ESM 4105", "AOE 4065"};
-  // green engineering
-  private String[] green = {"ENGR 3124", "ENGR 4134", "BC 3014", "CHE 3144", "AAEC 3314", "ARCH 4055"};
-  // naval
-  private String[] naval = {"AOE 2204", "AOE 4264", "AOE 4244", "AOE 3134", "AOE 3154", "ECE 3054"};
-  // nuclear
-  private String[] nuclear = {"MATH 2214", "NSEG 3145", "NSEG 3146", "NSEG 3604", "NSEG 4204", "NSEG 4974"};
+
+private String[] biomed = {
+    "BMES 2104 - Introduction to Biomedical Engineering",
+    "BMES 4064 - Introduction to Medical Physiology",
+    "BMES 3004 - Helmet Design: Biomechanics to Health & Social Disparities in Sports",
+    "BMES 3124 - Introduction to Biomechanics",
+    "BMES 3134 - Introduction to Biomedical Imaging",
+    "BMES 3144 - Biomedical Devices",
+    "BMES 3154 - Biosignal Processing and Classification",
+    "BMES 3164 - Fundamentals of Regenerative Medicine and Tissue Engineering",
+    "BMES 3224 - Automobile Safety",
+    "BMES 3704 - Computer Aided Design for Biomedical Engineering Applications",
+    "BMES 4034 - Wearable Bioinstrumentation",
+    "BMES 4084 - Cellular and Molecular Technologies in Biomedical Engineering",
+    "BMES 4134 - Global, Societal, and Ethical Considerations in Biomedical Engineering",
+    "BMES 4154 - Commercialization of BME Res"
+};
+
+
+// computer science
+
+private String[] cs = {
+    "CS 1114 - Introduction to Software Design",
+    "CS 2114 - Software Design and Data Structures",
+    "MATH 2534 - Introduction to Discrete Mathematics",
+    "CS 2505 - Introduction to Computer Organization",
+    "CS 3114 - Data Structures and Algorithms",
+    "CS 3214 - Computer Systems",
+    "CS 4104 - Data and Algorithm Analysis"
+};
+
+
+// cybersecurity
+
+private String[] cyber = {
+    "ECE 4560 - Computer and Network Security Fundamentals",
+    "CS 4264 - Principles of Computer Security",
+    "ECE/CS 5560 - Fundamentals of Info Security",
+    "ECE 3564 - Introduction to Computer Networking",
+    "CS 3214 - Computer Systems",
+    "ACIS 4684 - Information Systems Security and Assurance",
+    "CS 4274 - Secure Computing Capstone",
+    "ECE 4944 - Cybersecurity Seminar"
+};
+
+
+// human-computer interaction
+
+private String[] hci = {
+    "HIST/SOC/STS 2604 - Introduction to Data in Social Context",
+    "COMM 2084 - Media and Society",
+    "CS 3724 - Introduction to Human-Computer Interaction",
+    "IDS 2214 - IDS Studio for Minors",
+    "CS 3714 - Mobile Software Development",
+    "CS 4774 - Human-Computer Interaction Design Experience"
+};
+
+
+// artificial intelligence
+
+private String[] ai = {
+    "CS 1114 - Introduction to Software Design",
+    "CS 2104 - Introduction to Problem Solving in Computer Science",
+    "CS 3804 - Foundations and Applications of Artificial Intelligence",
+    "PHIL 3334 - Ethical Perspectives on Artificial Intelligence",
+    "GEOG 4094 - Generative AI Applications in Social Science",
+    "MKTG 4114 - Introduction to AI in Marketing"
+};
+
+
+// engineering science and mechanics
+
+private String[] esm = {
+    "ESM 2104 - Statics",
+    "ESM 2204 - Mechanics of Deformable Bodies",
+    "ESM 2304 - Dynamics",
+    "ESM 3234 - Fluid Mechanics I-Control Volume Analysis",
+    "ESM 3124 - Dynamics II - Analytical and 3-D Motion",
+    "ESM 4105 - Engineering Analysis of Physiologic Systems",
+    "AOE 4065 - Air Vehicle Design"
+};
+
+
+// green engineering
+
+private String[] green = {
+    "ENGR 3124 - Introduction to Green Engineering",
+    "ENGR 4134 - Environmental Life Cycle Assessment",
+    "BC 3014 - Building Physics and Environmental Systems",
+    "CHE 3144 - Mass Transfer",
+    "AAEC 3314 - Environmental Law",
+    "ARCH 4055 - Environment and Building Systems"
+};
+
+
+// naval engineering
+
+private String[] naval = {
+    "AOE 2204 - Introduction to Ocean Engineering",
+    "AOE 4264 - Principles of Naval Engineering",
+    "AOE 4244 - Naval and Marine Engineering Systems Design",
+    "AOE 3134 - Air Vehicle Dynamics",
+    "AOE 3154 - Astromechanics",
+    "ECE 3054 - Electrical Theory"
+};
+
+
+// nuclear engineering
+
+private String[] nuclear = {
+    "MATH 2214 - Introduction to Differential Equations",
+    "NSEG 3145 - Fundamentals of Nuclear Engr",
+    "NSEG 3146 - Fundamental of Nuclear Engr",
+    "NSEG 3604 - Radiation Detection, Protection and Shielding",
+    "NSEG 4204 - Nuclear Fuel Cycle",
+    "NSEG 4974 - Independent Study"
+};
+
 
   private String[] courses = {};
 
